7130aa6678442ea41b8e5be717d8e30879064080
Monet <codem0n3t@gmail.com>
2025-07-22 05:42:45 -0400

    Login and Registration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,59 @@
 import { useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
 import "./styles/App.css";
+import * as auth from "../utils/auth";
 
 function App() {
+  const [userData, setUserData] = useState({ username: "", email: "" });
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleRegistration = ({
+    username,
+    email,
+    password,
+    confirmPassword,
+  }) => {
+    if (password === confirmPassword) {
+      auth
+        .register(username, password, email)
+        .then(() => {
+          // handle succesful registration
+          navigate("/login");
+        })
+        .catch(console.error);
+    }
+  };
+  // handleLogin accepts one parameter: an object with two properties.
+  const handleLogin = ({ username, password }) => {
+    // If username or password empty, return without sending a request.
+    if (!username || !password) {
+      return;
+    }
+
+    // We pass the username and password as positional arguments. The
+    // authorize function is set up to rename `username` to `identifier`
+    // before sending a request to the server, because that is what the
+    // API is expecting.
+    auth
+      .authorize(username, password)
+      .then((data) => {
+        // Verify that a jwt is included before logging the user in.
+        if (data.jwt) {
+          setUserData(data.user); // save user's data to state
+          setIsLoggedIn(true); // log the user in
+          navigate("/ducks"); // send them to /ducks
+        }
+      })
+      .catch(console.error);
+  };
   return (
     <Routes>
       <Route
@@ -24,7 +68,7 @@ function App() {
         path="/my-profile"
         element={
           <ProtectedRoute isLoggedIn={isLoggedIn}>
-            <MyProfile />
+            <MyProfile userData={userData} />
           </ProtectedRoute>
         }
       />
@@ -33,15 +77,16 @@ function App() {
         path="/login"
         element={
           <div className="loginContainer">
-            <Login />
+            <Login handleLogin={handleLogin} />
           </div>
         }
       />
+
       <Route
         path="/register"
         element={
           <div className="registerContainer">
-            <Register />
+            <Register handleRegistration={handleRegistration} />
           </div>
         }
       />
